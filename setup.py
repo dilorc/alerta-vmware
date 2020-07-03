7f154a3c430381a11053d52c520314c25994754f
dilorc <cdilorenzo@gmail.com>
2020-07-02 21:48:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ setup(
     author='Your name',
     author_email='your.name@example.com',
     packages=find_packages(),
-    py_modules=['alerta-vmware'],
+    py_modules=['alerta_vmware'],
     install_requires=[],
     include_package_data=True,
     zip_safe=True,
     entry_points={
         'alerta.plugins': [
-            'vmware = alerta-vmware:VmwareTrapTransformer'
+            'vmware = alerta_vmware:VmwareTrapTransformer'
         ]
     }
 )
