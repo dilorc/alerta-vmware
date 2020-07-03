2912139f7a9bd917f9ec38e8ba487a95585ad4e0
dilorc <cdilorenzo@gmail.com>
2020-07-02 22:14:57 -0400

    Update alerta_vmware.py

--- alerta_vmware.py
+++ alerta_vmware.py
@@ -8,7 +8,17 @@ class VmwareTrapTransformer(PluginBase):
     def pre_receive(self, alert):
 
         LOG.info(alert.attributes['trapvars'])
-
+        alert.resource = alert.attributes['trapvars']['_5']
+        alert.event = alert.attributes['trapvars']['_4'].split('-')[0].strip()
+        alert.text = alert.attributes['trapvars']['_4']
+        alert.origin = alert.attributes['trapvars']['_A']
+        if alert.attributes['trapvars']['_3'] == "Yellow":
+            severity =  "minor"
+        elif alert.attributes['trapvars']['_3'] == "Red":
+            severity = "major"
+        elif alert.attributes['trapvars']['_3'] == "Green":
+            severity = "cleared"
+        alert.severity = severity
         return alert
 
     def post_receive(self, alert):
